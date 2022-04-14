bb823f8f2995c1f1d53841e8353fcff2074be1fe
NoVolkov <69715883+NoVolkov@users.noreply.github.com>
2022-04-14 08:30:59 +0300

    Start branch

--- MusicTest/app/src/main/java/ru/novlk/musictest/MainActivity.java
+++ MusicTest/app/src/main/java/ru/novlk/musictest/MainActivity.java
@@ -1,14 +1,162 @@
 package ru.novlk.musictest;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener, SoundPool.OnLoadCompleteListener {
+
+    MediaPlayer mPlayer;
+    Button playButton, pauseButton, stopButton;
+    SeekBar volumeControl;
+    AudioManager audioManager;
+
+    SoundPool sp;
+    int soundIdShot;
+    int soundIdExplosion;
+
+
+    int streamIDShot;
+    int streamIDExplosion;
+
+    String kickstart="https://ru.hitmotop.com/get/music/20170904/Mtley_Cre_-_Kickstart_My_Heart_48136615.mp3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context=this;
+        mPlayer= MediaPlayer.create(context,R.raw.joel_nielsen_questionable_ethics_1);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopPlay();
+                mPlayer.release();
+                mPlayer=new MediaPlayer();
+                mPlayer.setAudioAttributes(
+                        new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+                );
+                try {
+                    mPlayer.setDataSource(kickstart);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    mPlayer.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        playButton = findViewById(R.id.playButton);
+        pauseButton = findViewById(R.id.pauseButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        volumeControl = findViewById(R.id.volumeControl);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(curValue);
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(false);
+
+        sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        sp.setOnLoadCompleteListener(this);
+
+        soundIdShot = sp.load(this, R.raw.shot, 1);
+
+        soundIdExplosion = sp.load(this,R.raw.explosion,1);
+
+    }
+
+    public void onClick(View view) {
+        sp.play(soundIdShot, 1, 1, 0, 0, 1);
+        sp.play(soundIdExplosion, 1, 1, 0, 0, 1);
+    }
+
+    private void stopPlay(){
+        mPlayer.stop();
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(false);
+        try {
+            mPlayer.prepare();
+            mPlayer.seekTo(0);
+            playButton.setEnabled(true);
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void play(View view){
+
+        mPlayer.start();
+        playButton.setEnabled(false);
+        pauseButton.setEnabled(true);
+        stopButton.setEnabled(true);
+    }
+    public void pause(View view){
+
+        mPlayer.pause();
+        playButton.setEnabled(true);
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(true);
+    }
+    public void stop(View view){
+        stopPlay();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mPlayer.isPlaying()) {
+            stopPlay();
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mPlayer.start();
+    }
+
+    @Override
+    public void onLoadComplete(SoundPool soundPool, int i, int i1) {
+        System.out.println("+++++++++++++");
     }
 }
